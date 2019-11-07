f868bbabd009c3712ad0e97769e7ad7a1a5e8d33
klui_tammapon.N <kluy2000@gmail.com>
2019-11-07 21:17:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/test4/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.test4;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    public DatabaseReference myRef;
+    private TextView mFirebaseTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFirebaseTextView = (TextView)findViewById(R.id.firebaseTextView);
+        // Get firebase database ref
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef =database.getReference();
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Map map = (Map)dataSnapshot.getValue();
+                String x = String.valueOf(map.get("testcode"));
+                mFirebaseTextView.setText(x);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void onClickAddButton(View view) {
+
+        Map<String, Object> value = new HashMap<String, Object>();
+        value.put("testcode","Lnwsoftdev");
+        myRef.updateChildren(value);
+        myRef.child("testcode").child("Lnwsoftdev").setValue("001");
+        myRef.child("testcode").child("Noobsoftdev").setValue("002");
+    }
+}
