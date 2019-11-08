1c3910a9e5922f17ce1c72c79fde1103c2a43f07
OMEN <Bong.pannatorn@gmail.com>
2019-11-08 21:42:02 +0700

    update 2

--- app/src/main/java/com/example/test4/page_register.java
+++ app/src/main/java/com/example/test4/page_register.java
@@ -15,12 +15,10 @@ public class page_register extends AppCompatActivity {
     CheckBox checkbox_A,checkbox_ME;
     EditText id_regis,pass_regis,name_regis,mail_regis,phone_regis;
     Button create_register;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_register);
-
         create_register=(Button)findViewById(R.id.button_create_regis);
         checkbox_A=(CheckBox)findViewById(R.id.checkBox_admin);
         checkbox_ME=(CheckBox)findViewById(R.id.checkBox_manager_employee);
@@ -29,7 +27,6 @@ public class page_register extends AppCompatActivity {
         name_regis=(EditText)findViewById(R.id.name_regis);
         mail_regis=(EditText)findViewById(R.id.mail_regis);
         phone_regis=(EditText)findViewById(R.id.phone_regis);
-
         create_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
