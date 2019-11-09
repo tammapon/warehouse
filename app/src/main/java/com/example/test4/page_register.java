63a4a6debe428d80dde9ed1ff2790b3a101f9aaa
renedes <36676@chs.ac.th>
2019-11-09 21:06:37 +0700

    Revert "fi"
    This reverts commit 132c3922fcc46ef0bcd2f6f9b49c50e46e210825.

--- app/src/main/java/com/example/test4/page_register.java
+++ app/src/main/java/com/example/test4/page_register.java
@@ -10,25 +10,17 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class page_register extends AppCompatActivity {
-    public DatabaseReference myRef;
-    //private DatabaseReference mDatabase;
-
     Boolean check_A = false,check_B  =false;
     CheckBox checkbox_A,checkbox_ME;
     EditText id_regis,pass_regis,name_regis,mail_regis,phone_regis;
     Button create_register;
-    String bufferID,bufferName,bufferPass,buffermail,bufferphone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_register);
 
-        myRef = FirebaseDatabase.getInstance().getReference();
         create_register=(Button)findViewById(R.id.button_create_regis);
         checkbox_A=(CheckBox)findViewById(R.id.checkBox_admin);
         checkbox_ME=(CheckBox)findViewById(R.id.checkBox_manager_employee);
@@ -42,15 +34,6 @@ public class page_register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //send data to datbase
-                bufferID = (String)id_regis.getText().toString();
-                bufferName = (String)name_regis.getText().toString();
-                bufferPass = (String)pass_regis.getText().toString();
-                bufferphone = (String)phone_regis.getText().toString();
-                buffermail = (String)mail_regis.getText().toString();
-                myRef.child("user").child(bufferID).child("name").setValue(bufferName);
-                myRef.child("user").child(bufferID).child("email").setValue(buffermail);
-                myRef.child("user").child(bufferID).child("password").setValue(bufferPass);
-                myRef.child("user").child(bufferID).child("phone").setValue(bufferphone);
                 if(check_A.equals(true)){
                     startActivity(new Intent(page_register.this,page_login.class));
                 }
@@ -58,8 +41,7 @@ public class page_register extends AppCompatActivity {
                     startActivity(new Intent(page_register.this,page_levelSelect.class));
                 }
                 else {
-                    startActivity(new Intent(page_register.this,page_login.class));
-                    //Toast.makeText(getApplication(),"please select level!!!",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplication(),"please select level!!!",Toast.LENGTH_LONG).show();
                 }
             }
         });
