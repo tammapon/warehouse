00a1e66d178444bf3f3cccaa4348947c9c4618ed
NapatYodruk <57149903+NapatYodruk@users.noreply.github.com>
2019-11-10 13:58:09 +0700

    update by yala -page admin backEnd success full

---
+++ app/src/main/java/com/example/test4/page_showID.java
@@ -0,0 +1,28 @@
+package com.example.test4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class page_showID extends AppCompatActivity {
+    Button btn_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_show_id);
+
+        btn_back=(Button)findViewById(R.id.btn_back_showid);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent test = new Intent(page_showID.this,page_admin.class);
+                startActivity(test);
+            }
+        });
+    }
+}
