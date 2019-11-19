2c1e646378acebeedbd4e1a749c58365171ccb3a
NapatYodruk <57149903+NapatYodruk@users.noreply.github.com>
2019-11-20 05:16:18 +0700

    -page employee + scanQR

--- app/src/main/java/com/example/test4/add_product.java
+++ app/src/main/java/com/example/test4/add_product.java
@@ -1,15 +1,36 @@
 package com.example.test4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class add_product extends AppCompatActivity {
+    Button b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+
+        b1 = (Button)findViewById(R.id.back1);
+        b2 = (Button)findViewById(R.id.backok);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(add_product.this,manager_product.class));
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(add_product.this,manager_product.class));
+            }
+        });
     }
 
 
