274273b1b85821e2b32da70da025d5d58c558662
OMEN <Bong.pannatorn@gmail.com>
2019-11-20 09:33:54 +0700

    update 2

--- app/src/main/java/com/example/test4/add_product.java
+++ app/src/main/java/com/example/test4/add_product.java
@@ -8,22 +8,15 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class add_product extends AppCompatActivity {
-    Button b1,b2;
+    Button b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
 
-        b1 = (Button)findViewById(R.id.back1);
         b2 = (Button)findViewById(R.id.backok);
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(add_product.this,manager_product.class));
-            }
-        });
 
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
