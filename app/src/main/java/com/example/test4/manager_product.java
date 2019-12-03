84bafa4233662187c78ca7a5b05e5366d4fa42d8
Poomchatbodin <poom_1414@hotmail.com>
2019-12-04 03:56:19 +0700

    update popup

--- app/src/main/java/com/example/test4/manager_product.java
+++ app/src/main/java/com/example/test4/manager_product.java
@@ -41,6 +41,7 @@ public class manager_product extends AppCompatActivity {
     private ArrayList<Integer> value = new ArrayList<>();
     private ArrayList<String> data_check = new ArrayList<String>();
     private String check_data = "620106";
+    Dialog myDialog;
 
     Button btn_tozone,btn_toaddproduct;
     private DatabaseReference myRef;
@@ -48,6 +49,7 @@ public class manager_product extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager_product);
+        myDialog = new Dialog(this);
 
        btn_toaddproduct=(Button)findViewById(R.id.btnToaddZone);
        btn_tozone=(Button)findViewById(R.id.btnToZone);
@@ -131,6 +133,20 @@ public class manager_product extends AppCompatActivity {
         adapter = new CustomListView(this);
         swipeListView.setAdapter(adapter);
 
+    }
+
+    public void ShowPopup(View v){
+
+        Button btncancle;
+        myDialog.setContentView(R.layout.windowpopup);
+        btncancle = (Button) myDialog.findViewById(R.id.btncancle);
+        btncancle.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                myDialog.dismiss();
+            }
+        });
+        myDialog.show();
     }
 
     private void setDataListView(String ID,String NAME,String ZONE,int AMOUNT,String date,int Value) {
